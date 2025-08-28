5f1a322fdc1c9186ea473b498c88a9c598fa1849
rashel9255 <ibrahim2000167@gmail.com>
2025-08-29 02:25:58 +0600

    calling service functionality add

--- script.js
+++ script.js
@@ -5,9 +5,33 @@ const cardHearts = document.getElementsByClassName("card-heart");
 
 let count = 0;
 
-for (let i = 0; i < cardHearts.length; i++) {
-  cardHearts[i].addEventListener("click", function() {
+for (let heart of cardHearts) {
+  heart.addEventListener("click", function() {
     count++;
     heartCount.innerText = count;
   });
+}
+
+// call button functionality
+
+const callButtons = document.getElementsByClassName("call-button");
+
+let coins = parseInt(document.getElementById('coin-number').innerText);
+
+const serviceNames = document.getElementsByClassName('service-name');
+
+const serviceNumbers = document.getElementsByClassName('service-number');
+
+for(let i =0; i< callButtons.length;i++){
+    callButtons[i].addEventListener('click',function(){
+        if(coins<20){
+           return alert('Do not have sufficient coin');
+        }
+        coins -= 20;
+        document.getElementById('coin-number').innerText = coins;
+        const serviceName = serviceNames[i].innerText;
+        const serviceNumber = serviceNumbers[i].innerText;
+        alert('📞 calling ' + serviceName +' '+ serviceNumber);
+        
+    })
 }
