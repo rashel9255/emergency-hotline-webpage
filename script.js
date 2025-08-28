24442e2294e1e9d2e09d1aa7b6140970624522fa
rashel9255 <ibrahim2000167@gmail.com>
2025-08-29 02:59:10 +0600

    call history update add

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ const serviceNames = document.getElementsByClassName('service-name');
 
 const serviceNumbers = document.getElementsByClassName('service-number');
 
+const callHistoryContainer = document.getElementById('call-history-container');
+
 for(let i =0; i< callButtons.length;i++){
     callButtons[i].addEventListener('click',function(){
         if(coins<20){
@@ -32,6 +34,20 @@ for(let i =0; i< callButtons.length;i++){
         const serviceName = serviceNames[i].innerText;
         const serviceNumber = serviceNumbers[i].innerText;
         alert('📞 calling ' + serviceName +' '+ serviceNumber);
-        
+
+        // call history create 
+
+        const time = new Date().toLocaleTimeString();
+        const div =document.createElement('div')
+        div.innerHTML = `
+          <div class="flex justify-between items-center mt-2 p-2 bg-[#FAFAFA] rounded-lg">
+                    <div>
+                        <h1>${serviceName}</h1>
+                        <p class="text-gray-500">${serviceNumber}</p>
+                    </div>
+                    <div>${time}</div>
+                </div>
+        `
+        callHistoryContainer.appendChild(div);
     })
 }
